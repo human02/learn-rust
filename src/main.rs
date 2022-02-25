ff4e8d6002b9a9c5e542164ef396f4733deb6d32
Surya-Dev <aaravchandra02@gmail.com>
2022-02-25 08:51:54 -0800

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 fn main() {
-    let x = 10;
-    println!("\nHello - World\nx = {}", x);
-    let mut y = 1;
-    println!("\ncurrently y = {}", y);
-    y = y + x;
-    println!("\nNow y is mutable and its value is now = {}", y);
-    let z = '\u{1F595}'; // using unicode value s char is stored in 4 byte instead of 1 byte in Rust
-    println!(" Hello Peeps its time for you all to {}  ", z);
+    /* Array (in Rust) :
 
-    let a1 = 13;
-    let a2 = 2.3;
-    let a3: f32 = 120.0;
+    - Collection of elements with the same data type
+    - Fixed length
+    - Stored in contiguous memory location
+    - Stored in order
+    - has zero (0) indexing like many other languages
+    */
 
-    let avg = (a1 as f64 + a2 + a3 as f64) / 3.0; // Type casting using "as". arithmetic operations cant be done on different datatype variables even f32 and f64 is different.
-    println!("average of a1,a2,a3 = {}", avg);
-    assert_eq!(avg, 45.1);
-    println!("\n Correct avg generated");
+    let mut letters = ['a', 'b', 'c'];
+    let mut first_lttr = letters[0];
+    println!("first letter is --> {}", first_lttr);
+    letters[0] = 'z';
+    first_lttr = letters[0];
+    println!("changed the first letter to --> {}", first_lttr);
+
+    // Rust allows array decalaration w/o initial values.
+    let nums: [i32; 7]; // here nums is of i32 type and has fixed length of 7
+    nums = [1, 1, 1, 1, 1, 1, 1];
+    let more_nums: [i32; 100]; // can't initialise this array the same way.
+    more_nums = [1; 100]; // using repeat expression to initialise now.
+
+    /*
+     Index variable will be of 'usize' datatype and its based on no. of bytes needed to reference memory.
+     Compiling for 32 bit processor - usize is 4 bytes
+     Compiling for 64 bit processor - usize is 8 bytes
+    */
+    let index: usize = more_nums.len();
+    println!(
+        "Last number in more_nums array is {}\n",
+        more_nums[index - 1]
+    );
 }
